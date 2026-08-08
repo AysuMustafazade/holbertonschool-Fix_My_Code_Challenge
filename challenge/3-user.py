8969d56509aeea06c201c11dc818a9d0c203416d
AysuMustafazade <mustafazadays16@gmail.com>
2026-08-09 02:40:23 +0400

    done

---
+++ challenge/3-user.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+User Model
+"""
+import hashlib
+import uuid
+
+
+class User:
+    """
+    User class:
+    - id: public string unique (uuid)
+    - password: private string hash in MD5
+    """
+
+    __password = None
+
+    def __init__(self):
+        """
+        Initialize a new user:
+        - assigned an unique `id`
+        """
+        self.id = str(uuid.uuid4())
+
+    @property
+    def password(self):
+        """
+        Password getter
+        """
+        return self.__password
+
+    @password.setter
+    def password(self, pwd):
+        """
+        Password setter
+        """
+        if pwd is None or type(pwd) is not str:
+            self.__password = None
+        else:
+            self.__password = hashlib.md5(pwd.encode()).hexdigest()
+
+    def is_valid_password(self, pwd):
+        """
+        Validate password
+        """
+        if pwd is None or type(pwd) is not str:
+            return False
+
+        if self.__password is None:
+            return False
+
+        return hashlib.md5(pwd.encode()).hexdigest() == self.__password
